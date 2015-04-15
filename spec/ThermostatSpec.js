924afb1a9e48c2193f1248832a9a887690a6013a
Maciej Kurek <maciejk77@gmail.com>
2015-04-15 11:53:12 +0100

    Thermostat engine, without POST bit

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,83 @@
+describe('Thermostat', function(){
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
+  describe('can change temperature', function(){
+
+    it('can have a default temperature', function(){
+      expect(thermostat.temperature).toEqual(20);
+    });
+
+    it('can increase the temperature with the up function', function() {
+      thermostat.up();
+      expect(thermostat.temperature).toEqual(21);
+    });
+
+    it('can decrease the temperature with down function', function() {
+      thermostat.down();
+      expect(thermostat.temperature).toEqual(19);      
+    });
+
+    it('can reset temperature to 20', function(){
+      thermostat.temperature = 21;
+      thermostat.resetTemp();
+      expect(thermostat.temperature).toEqual(20);
+    });
+
+  });
+
+  describe('can have max or min temperature', function(){
+
+    it('has a minimum temperature of 10', function(){
+      thermostat.temperature = 10;
+      expect( function(){ thermostat.down(); } ).toThrow(new Error('10 is the minimum'));
+    });
+
+    it('has max temperature of 25, when powersaving is on', function(){
+      thermostat.powersaving = true;
+      thermostat.temperature = 25;
+      expect( function(){ thermostat.up(); } ).toThrow(new Error('Max 25 degrees when powersaving is on'));
+    });
+
+    it('has max temperature of 32, when powersaving is off', function(){
+      thermostat.powersaving = false;
+      thermostat.temperature = 32;
+      expect( function(){ thermostat.up(); } ).toThrow(new Error('Max 32 degrees when powersaving is off'));
+    });
+
+  });
+
+  describe('can change the displays colour', function(){
+
+    it('changes to green when temperature is below 18', function(){
+      thermostat.temperature = 18;
+      thermostat.down();
+      expect(thermostat.tempColour).toEqual('Green');
+    });
+
+    it('changes to yellow when temperature rises over 18', function(){
+      thermostat.temperature = 17;
+      thermostat.tempColour = "Green";
+      thermostat.up();
+      expect(thermostat.tempColour).toEqual("Yellow");
+    });
+
+    it('changes to yellow when temperature falls below 25', function(){
+      thermostat.temperature = 26;
+      thermostat.tempColour = "Red";
+      thermostat.down();
+      expect(thermostat.tempColour).toEqual("Yellow");
+    });
+
+    it('changes to red when temperature rises above 25', function(){
+      thermostat.powersaving = false;
+      thermostat.temperature = 25;
+      thermostat.up();
+      expect(thermostat.tempColour).toEqual("Red");
+    });
+
+  });
+
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,40 @@
+var Thermostat = function() {
+  this.temperature = 20;
+  this.powersaving = true;
+  this.tempColour = "Yellow";
+};
+
+Thermostat.prototype.up = function(){
+  if(this.temperature === 25 && this.powersaving){
+    throw new Error('Max 25 degrees when powersaving is on');
+  };
+  if(this.temperature === 32){
+    throw new Error('Max 32 degrees when powersaving is off');
+  };
+  this.temperature ++;
+  this.colour();
+};
+
+Thermostat.prototype.down = function(){
+  if(this.temperature === 10){
+    throw new Error('10 is the minimum'); 
+  };
+  this.temperature --;
+  this.colour();
+};
+
+Thermostat.prototype.resetTemp = function(){
+  this.temperature = 20;
+};
+
+Thermostat.prototype.colour = function(){
+  if(this.temperature < 18){
+    this.tempColour = "Green";
+  };
+  if(this.temperature >= 18 && this.temperature <= 25){
+    this.tempColour = "Yellow";
+  };
+  if(this.temperature > 25){
+    this.tempColour = "Red";
+  };
+};
