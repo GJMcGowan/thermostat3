10210882f2eeae668f0d6e87f2f8f70ea641ef0f
GJMcGowan <gemcgow@gmail.com>
2015-04-15 13:41:16 +0100

    Minor refactoring to move functions into different files

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,18 +1,3 @@
-$(document).ready(function(){
-  
-  var thermostat = new Thermostat();
-  
-  $('#temperature').text(thermostat.temperature);
-  $('#up').click(function(){
-    thermostat.up();
-    $('#temperature').text(thermostat.temperature);
-  });
-  $('#down').click(function(){
-    thermostat.down();
-    $('#temperature').text(thermostat.temperature);
-  });
-});
-
 var Thermostat = function() {
   this.temperature = 20;
   this.powersaving = true;
